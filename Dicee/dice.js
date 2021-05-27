ec5e0d11aa6670e150556c74535760175d1c6ef5
Rititk Banerjee <banerjeeritik@gmail.com>
2021-05-27 12:57:27 +0530

    Add files via upload

---
+++ Dicee/dice.js
@@ -0,0 +1,16 @@
+var randomNumber1= Math.random()*6;
+randomNumber1=Math.floor(randomNumber1)+1;
+var randomNumber2= Math.random()*6;
+randomNumber2=Math.floor(randomNumber2)+1;
+
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent="Player 1 wins 🚩";
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").textContent="Player 2 wins 🚩";
+}
+else{
+    document.querySelector("h1").textContent="Draw";
+}
